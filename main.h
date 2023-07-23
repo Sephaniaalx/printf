5d8848384e74fc13ee10450f45244ca7f5f144fc
Sephaniaalx <koutanyi@moolre.com>
2023-07-22 23:46:36 -0700

    headerfiles

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * all prototypes and header files 
+ * are included in this main file
+ */
+
+/**
+ * below are few system header files
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * below are actual prototypes
+ */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif

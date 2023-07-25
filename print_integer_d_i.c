79897581a1e89cd96b46e7ad0757319252cded0a
David Akpan <Davidakpan247@gmail.com>
2023-07-25 05:42:39 -0700

    work in progress

--- print_integer_d_i.c
+++ print_integer_d_i.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * print_integer - the integer function
- * @print_f: the function parameter
- * Return: returns the number digits
- */
-
+* print_integer - The function to print
+* @print_f: The printf specifier
+* Return: The character specifier
+*/
 int print_integer(va_list print_f)
 {
 	int num = va_arg(print_f, int);
@@ -22,11 +22,11 @@ int print_integer(va_list print_f)
 		num_digits++;
 		temp /= 10;
 	} while (temp != 0);
-
 	for (i = num_digits - 1; i >= 0; --i)
 	{
-		int digit = num / power(10, i);
+	int digit = num / power(10, i);
 
-		num -= digit * power(10, i);
+	num -= digit * power(10, i);
 	}
-	return (num_digits);}
+	return (num_digits);
+}
